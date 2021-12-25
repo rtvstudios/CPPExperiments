d8114eedc01ca59088d1fb91475fbbc9eba63e27
Ranjith TV <ranjith.tv@gmail.com>
2021-12-25 14:24:46 -0500

    missing change

---
+++ concepts/array_sorter.h
@@ -0,0 +1,23 @@
+
+#ifndef ARRAY_SORTER_H
+#define ARRAY_SORTER_H
+
+#include <algorithm>
+#include <array>
+#include <concepts>
+
+template<class T>
+concept Sortable = requires(T a, T b) {
+        { a < b } -> std::convertible_to<bool>;
+};
+
+template <Sortable T, int n>
+constexpr auto sort(const T(&arr)[n]) {
+    std::array<T, n> res;
+    std::copy(std::begin(arr), std::end(arr), std::begin(res));
+    std::sort(std::begin(res), std::end(res));
+    return res;
+}
+
+
+#endif
