9620eb35fafc4ac8c0849cced3f4bce1c156ba9d
rtvstudios <ranjith.tv@gmail.com>
2022-01-07 19:23:34 -0500

    iterator added in range example

--- ranges/main.cpp
+++ ranges/main.cpp
@@ -2,6 +2,42 @@ import <iostream>;
 import <vector>;
 import <ranges>;
 import <initializer_list>;
+import <iterator>;
+
+template<typename T>
+class even_iterator {
+public:
+    using difference_type = std::ptrdiff_t;
+
+    even_iterator() = default;
+
+    T& operator*() {
+        return mVal;
+    }
+
+    const T& operator*() const {
+        return mVal;
+    }
+
+    even_iterator& operator++() {
+        mVal += 2;
+        return *this;
+    }
+
+    even_iterator operator++(int) {
+        auto res = *this;
+        ++(*this);
+        return res;
+    }
+
+    bool operator==(const even_iterator&) const = default;
+
+private:
+    T mVal{0};
+};
+
+template<typename T>
+constexpr auto even_view = std::ranges::subrange<even_iterator<T>, std::unreachable_sentinel_t>{};
 
 int main(int argc, char **argv) {
     auto even = [](int i) { return 0 == i % 2; };
@@ -13,7 +49,13 @@ int main(int argc, char **argv) {
 
         std::cout << i << ' ';
     }
+    std::cout << std::endl;
 
+    // iterator
+    for (auto i: even_view<int> |
+                 std::views::take(5)) {
+        std::cout << i << ' ';
+    }
     std::cout << std::endl;
 
     return 0;
