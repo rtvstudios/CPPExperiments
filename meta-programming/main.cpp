de21abd29bbbf8ee945b1f7b0f03c4d3a3bf510b
rtvstudios <ranjith.tv@gmail.com>
2022-02-01 11:11:32 -0500

    Minor update

--- meta-programming/main.cpp
+++ meta-programming/main.cpp
@@ -2,12 +2,12 @@ import <iostream>;
 import <initializer_list>;
 
 template<std::size_t N>
-struct fixed_str {
-    constexpr fixed_str(const char (&v)[N+1]) {
+struct FixedString {
+    constexpr FixedString(const char (&v)[N+1]) {
         std::copy_n(v, N+1, data);
     }
 
-    auto operator<=>(const fixed_str &) const = default;
+    auto operator<=>(const FixedString &) const = default;
 
     std::size_t size() const {
         return N;
@@ -15,68 +15,67 @@ struct fixed_str {
 
     char data[N+1] = {};
 
-    friend std::ostream& operator <<(std::ostream &out, const fixed_str &str) {
+    friend std::ostream& operator <<(std::ostream &out, const FixedString &str) {
         out << str.data;
         return out;
     }
 };
 
-template<std::size_t N> fixed_str(const char (&v)[N]) -> fixed_str<N-1>;
+template<std::size_t N> FixedString(const char (&v)[N]) -> FixedString<N-1>;
 
-template<fixed_str Name, typename Value>
-struct member {
-
+template<FixedString Name, typename Value>
+struct Member {
     constexpr static auto name() {
         return Name;
     }
     Value value;
 
-    friend std::ostream& operator <<(std::ostream &out, const member &v) {
+    friend std::ostream& operator <<(std::ostream &out, const Member &v) {
         out << "{\"" << v.name() << "\",\"" << v.value << "\"}";
         return out;
     }
 };
 
 template<typename... Members>
-struct meta_struct: public Members... {
+struct MetaStruct: public Members... {
 };
 
-template<fixed_str tag, typename T>
-decltype(auto) get_impl(const member<tag, T> &m) {
+template<FixedString tag, typename T>
+decltype(auto) getImpl(const Member<tag, T> &m) {
     return m;
 }
 
-template<fixed_str tag, typename meta_struct>
-decltype(auto) get(meta_struct &&s) {
-    return get_impl<tag>(std::forward<meta_struct>(s));
+template<FixedString tag, typename MetaStruct>
+decltype(auto) get(MetaStruct &&s) {
+    return getImpl<tag>(std::forward<MetaStruct>(s));
 }
 
-template<fixed_str tag, typename meta_struct>
-decltype(auto) get_value(meta_struct &&s) {
-    return get_impl<tag>(std::forward<meta_struct>(s)).value;
+template<FixedString tag, typename MetaStruct>
+decltype(auto) get_value(MetaStruct &&s) {
+    return getImpl<tag>(std::forward<MetaStruct>(s)).value;
 }
 
 int main(int, char **)
 {
     {
-        fixed_str name{"Fixed String"};
+        FixedString name{"Fixed String"};
         std::cout << name << std::endl;
     }
 
     {
-        member<"Age", int> age1{ 50 };
+        Member<"Age", int> age1{ 50 };
         std::cout << age1 << std::endl; 
     }
 
     {
-        meta_struct<member<"Name", std::string>, member<"Age", int>> data{"Jhon", 20};
+        using Person = MetaStruct<Member<"Name", std::string>, Member<"Age", int>>;
+        Person data{"Jhon", 20};
         std::cout << get<"Name">(data) << std::endl;
         std::cout << get<"Age">(data) << std::endl;
 
         std::cout << get_value<"Name">(data) << std::endl;
         std::cout << get_value<"Age">(data) << std::endl;
     }
-    
 
     return 0;
 }
